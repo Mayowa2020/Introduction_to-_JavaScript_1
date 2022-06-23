b8940e744e1fd53a66bf59f9a3013cb4634d02bc
Mayowa2020 <73591664+Mayowa2020@users.noreply.github.com>
2022-06-23 09:45:43 +0100

    creted a JS file to display my bio_data.

---
+++ bio_data.js
@@ -0,0 +1,7 @@
+let myName = "Adebukunola Oyetimehin";
+let height = "1.56metres";
+let country = "Nigeria";
+
+document.write(
+	`My name is ${myName}, I am ${height} tall and my home country is ${country}`
+);
